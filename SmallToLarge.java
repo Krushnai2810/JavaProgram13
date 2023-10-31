5114810fc27d9f87fb525457259f9be0104470c3
Krushnai jadhav <120713013+Krushnai2810@users.noreply.github.com>
2023-10-31 10:10:52 +0530

    Add files via upload

---
+++ SmallToLarge.java
@@ -0,0 +1,11 @@
+class SmallToLarge
+{
+public static void main(String args[])
+{
+int b=100;
+long l;
+l=b;
+System.out.println("b="+b);
+System.out.println("l="+l);
+}
+}
---
+++ SmallToLarge3.java
@@ -0,0 +1,9 @@
+class SmallToLarge3
+{
+public static void main(String args[])
+{
+byte b=50;
+System.out.println("b="+b);
+
+}
+}
